7a559ff27b076d15c36775902feda911c8582540
s2kfireworks <55568463+s2kfireworks@users.noreply.github.com>
2022-10-11 12:02:40 -0400

    endpoint update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 var orderNumber = '';
 var prevOrderType = '';
-const orderServiceUrl = "https://script.google.com/macros/s/AKfycbzKr8pcfm2nAOQ7K1y5q8yf2U-ka3GEfjJY99Tpq1v3KCLo8SQz4mO3mN-Qyq5d/exec";
+const orderServiceUrl = "https://script.google.com/macros/s/AKfycbyYSaORwV1ZWxYZ-_1FYf2My5OJr2T-rOafxPVPkbdySEnYoCZE_gpIO1s6DFDbgf4/exec";
 const orderConfUrl = "https://script.google.com/macros/s/AKfycbzOwXlcdjS-zU1qSl5p7Fx1yhYDaow2_mtg5z8ZkicU7E8CiWtaJ5CizJyAnpgo4Qdu/exec"; 
 
 $(window).on('load', function() { 
